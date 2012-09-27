41fae3579593adc92bf75f3109cf660f54d20d03
Leo Schweizer <leonhard.schweizer@googlemail.com>
2012-09-27 21:25:28 +0200

    some more test cases

--- src/test/scala/net/connectedcaching/sbcrypt/tests/SBCryptSpec.scala
+++ src/test/scala/net/connectedcaching/sbcrypt/tests/SBCryptSpec.scala
@@ -8,8 +8,12 @@ class SBCryptSpec extends Specification {
 
 	"sBCrypt" should {
 
-		"return the correct result when using a user-defined salt" in {
-			"foobar".bcrypt("$2a$10$XioEvBw/KFQo6kP0J5ttxe") must be equalTo("$2a$10$XioEvBw/KFQo6kP0J5ttxebYa6ujuMZLt5kWnvHOkIPiI7CYqzf3C")
+		"use a salt generated in 10 rounds per default" in {
+			"foobar".bcrypt must startWith("$2a$10$")
+		}
+
+		"use as many rounds as defined by the user for salting" in {
+			"foobar".bcrypt(5) must startWith("$2a$05$")
 		}
 
 		"identify valid hashes correctly" in {
@@ -19,12 +23,25 @@ class SBCryptSpec extends Specification {
 			"foobar".equalsBCrypted("$2a$10$XioEvBw/KFQo6kP0J5ttxeGuRTyMuNyipAR42vzf4ANGCnZ0n41Uy") must beFalse
 		}
 
-		"use a salt generated in 10 rounds per default" in {
-			"foobar".bcrypt() must startWith("$2a$10$")
+		"not deliver the same result twice if no seed is specified" in {
+			val a = "foobar".bcrypt
+			val b = "foobar".bcrypt
+			a must not be equalTo(b)
 		}
 
-		"use as many rounds as defined by the user for salting" in {
-			"foobar".bcrypt(5) must startWith("$2a$05$")
+		"always deliver the same result when using a user defined salt" in {
+			val a = "foobar".bcrypt("$2a$10$XioEvBw/KFQo6kP0J5ttxe")
+			val b = "foobar".bcrypt("$2a$10$XioEvBw/KFQo6kP0J5ttxe")
+			a must be equalTo(b)
+		}
+
+		"not deliver the same result twice when using a SecureRandom instance" in {
+			val prng = java.security.SecureRandom.getInstance("SHA1PRNG")
+			"foobar".bcrypt(10, prng) must not be equalTo("foobar".bcrypt(10, prng))
+		}
+
+		"return the correct result when using a user-defined salt" in {
+			"foobar".bcrypt("$2a$10$XioEvBw/KFQo6kP0J5ttxe") must be equalTo("$2a$10$XioEvBw/KFQo6kP0J5ttxebYa6ujuMZLt5kWnvHOkIPiI7CYqzf3C")
 		}
 
 	}
